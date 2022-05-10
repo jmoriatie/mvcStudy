9ae418711673136d96e346577a714ae7d5826c3b
jmoriatie <jeongmin9102@naver.com>
2022-05-10 22:43:04 +0900

    request response data 교환 servlet 과 연결하여 학습

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// cmd + 1 : 왼쪽 프로젝트 트리 쪽으로 이동(키보드로 클래스 선택하기 편함) + ESC 클릭시 빠져나옴
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: text/html;charset=utf-8
+        response.setContentType("text/html"); // html 을 입력해주는 게 정석
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("    <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+}
